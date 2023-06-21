b838269708171468f49efacd88caff8a3ef28e0a
Tlahui Bolaños <tabm96@gmail.com>
2023-06-21 11:04:34 -0600

    Built inference framework

--- solvers/inference.py
+++ solvers/inference.py
@@ -1,6 +1,10 @@
 import os
 import subprocess
 import numpy as np
+import torch
+from torch import tensor
+
+from models import MLP
 
 wd = os.getcwd()
 
@@ -9,22 +13,24 @@ def runGo(function: str, *args):
 
     return result.stdout.decode('utf-8')
 
-def decodeCubes(raw: str):
+def decodeCubes(raw: str, device: str = 'cpu'):
     rawSplit = raw.replace('[', '').replace(']', '').split('\n')[:-1]
 
     cubes = []
     embeds = []
-    diff = []
+    diff = np.array([])
     for item in rawSplit:
         itemSplit = item.split("|")
         cubes.append(itemSplit[0])
         embeds.append(np.array(itemSplit[1].split(' ')).astype(int))
 
         try:
-            diff.append(np.array(itemSplit[2].split(' ')).astype(int))
+            diff = np.append(diff, np.array(itemSplit[2].split(' ')).astype(int))
         except IndexError:
             pass
-
+    
+    diff = diff.reshape((-1, 282))
+    diff = tensor(diff, dtype=torch.float, device=device, requires_grad=False)
 
     return cubes, embeds, diff
 
@@ -34,19 +40,81 @@ def receiveRandomCube(nMoves: int):
     cubes, embeds, _ = decodeCubes(raw)
     return cubes[0], embeds[0]
 
-def getPossibleMoves(cube: str):
+def getPossibleMoves(cube: str, **kwargs):
     raw = runGo('getPossibleMoves', cube)
-    cubes, embeds, diff = decodeCubes(raw)
+    cubes, embeds, diff = decodeCubes(raw, **kwargs)
     return cubes, embeds, diff
 
 if __name__ == '__main__':
-    nMoves = np.random.randint(1, 25)
+    # Setup the inference model
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = MLP(282, 12, 6).to(device)
+    model.load_state_dict(torch.load(f'{wd}/constructor/solves/v8/Checkpoints/model.pth'))
+    model.eval()
+
+    # Get a cube to solve
+    # nMoves = np.random.randint(1, 25)
+    nMoves = 5
     originalCube, originalEmbed = receiveRandomCube(nMoves)
-    print(originalEmbed)
 
-    queue = [[originalEmbed]]
+    if all(originalEmbed[-6:] == [1, 1, 1, 1, 1, 1]):
+        print('Original cube is already solved')
 
-    cubes, embeds, diff = getPossibleMoves(originalCube)
-    print(len(embeds))
-    print(len(diff[0]))
+    # Initialize queue
+    cubeQueue = [[originalCube]]
+    probQueue = [[0]]
+    positionDict = {''.join(originalEmbed.astype(str)): 0}
+
+    # Try moves until solved
+    while True:
+        # Get the position to expand
+        while True:
+            if not probQueue: break
+            if not probQueue[-1]:
+                probQueue = probQueue[:-1]
+                cubeQueue = cubeQueue[:-1]
+            else: break
+        if not probQueue: break
+        foundSolve = False
+        queuePos = -1
+        index = np.argmin(probQueue[queuePos])
+        cubes, embeds, diff = getPossibleMoves(cubeQueue[-1][index], device=device)
+        probs = model(diff).cpu().detach().numpy().T
+        
+        probBuffer = []
+        cubeBuffer = []
+        for i in range(18):
+            try:
+                positionDict[''.join(embeds[i].astype(str))]
+            except KeyError:
+                positionDict[''.join(embeds[i].astype(str))] = len(probQueue)
+                probBuffer.append(probs[0][i])
+                cubeBuffer.append(cubes[i])
+
+                if all(embeds[i][-6:] == [1, 1, 1, 1, 1, 1]):
+                    print(f'Solved cube in {len(probQueue)} moves.')
+                    nMoves = len(probQueue)
+                    probQueue = probQueue[:-2]
+                    cubeQueue = cubeQueue[:-2]
+                    foundSolve = True
+                    break
+        
+        if foundSolve: continue
+
+        if probBuffer and len(probQueue) < nMoves:
+            probQueue.append(probBuffer)
+            cubeQueue.append(cubeBuffer)
+            queuePos = -2
+
+        probQueue[queuePos].pop(index)
+        cubeQueue[queuePos].pop(index)
+
+        if not probQueue: break
+
+
+
+    
+
+
+        
 
